e296a5671aa4a3063fd80ab61a0ce5e7a4fe4c32
Ashish yadav <ashishyadav@sigmoidanalytics.com>
2022-01-15 19:04:21 +0530

    one last commit

--- sample.py
+++ sample.py
@@ -1,30 +1,7 @@
-import flask
-from flask import Flask, render_template, Response
 import time
 import psutil
-
-
-app = Flask(__name__)
-
-
-def fun():
-    x =  psutil.cpu_percent(1)
-    y =  psutil.virtual_memory()[2]
-    return x, y;
-
-
-@app.route('/content')
-def content():
-    def inner():
-        while True:
-            cpu, mem = fun()
-            yield 'CPU usage: ' +  str(cpu) + '--------' + ' Memory usage: ' + str(mem) + '<br/>\n'
-            time.sleep(10)
-    return Response(inner(), mimetype='text/html')
-
-@app.route('/')
-def index():
-    return render_template('index.html.jinja')
-
-
-app.run(debug=True)
+print('Hello World')
+while True:
+    print('CPU:', psutil.cpu_percent(1))
+    print('Mem usage:', psutil.virtual_memory()[2])
+    time.sleep(10)
